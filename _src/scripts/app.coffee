c5aff12557473665270d64cea3a90920c55df267
anson0370 <anson0370@gmail.com>
2013-09-29 00:57:39 +0800

    console output for fun

--- _src/scripts/app.coffee
+++ _src/scripts/app.coffee
@@ -11,3 +11,6 @@ $ ->
 
   # init all github_commits
   require("github_commits").embedCommits $(".js_github_commits")
+
+  # console output for chrome
+  console.log("%cWelcome%chttp://anson.so/etc/jd", "line-height: 30px; border-radius: 5px 0 0 5px; background-color: #666; color: white; padding: 6px;", "border-radius: 0 5px 5px 0; border: 1px solid #666; color: #666; padding: 5px;")
--- public/js/app.js
+++ public/js/app.js
@@ -69,7 +69,8 @@
       }
     });
     $(window).scroll();
-    return require("github_commits").embedCommits($(".js_github_commits"));
+    require("github_commits").embedCommits($(".js_github_commits"));
+    return console.log("%cWelcome%chttp://anson.so/etc/jd", "line-height: 30px; border-radius: 5px 0 0 5px; background-color: #666; color: white; padding: 6px;", "border-radius: 0 5px 5px 0; border: 1px solid #666; color: #666; padding: 5px;");
   });
 
 }).call(this);
